82e619edf444efce051c5c9a08530a1bbda9fc2f
Eduardo Rauber Silva <1658928@sga.pucminas.br>
2026-05-04 11:00:35 -0300

    Finalização do ex2 AP09

---
+++ atividades_praticas/AP09/ex2/principal_viagem.py
@@ -0,0 +1,23 @@
+import funcoes_viagem as funcoes
+
+valor_base = int(input("Digite o valor base da passagem: "))
+bagagem = int(input("Digite a taxa da bagagem: "))
+passagem = funcoes.calcular_passagem(valor_base , bagagem)
+
+valor_diarias = int(input("Digite o valor da diária: "))
+dias = int(input("Digite a quantidade de dias: "))
+taxa_extra = int(input("Digite o valor da taxa extra da hospedagem: "))
+hospedagem = funcoes.calcular_hospedagem( valor_diarias , dias , taxa_extra)
+
+total_horas = int(input("Digite a duração total (em horas) da viagem: "))
+total_dias , sobra_horas = funcoes.converter_duracao(total_horas)
+
+alimentacao = int(input("Digite o gasto com alimentação: "))
+orcamento_total = funcoes.calcular_orcamento(passagem , hospedagem , alimentacao)
+
+print(f"Valor final da passagem: R$ {passagem}.")
+print(f"Valor final da hospedagem: R$ {hospedagem}.")
+print(f"A viagem dura {total_dias} dias e {sobra_horas} horas.")
+print(f"A viagem tem um custo fixo de R$ {orcamento_total[0]}.")
+print(f"A viagem tem um custo extra de R$ {orcamento_total[1]}.")
+print(f"A viagem tem um custo total de R$ {orcamento_total[3]}.")
