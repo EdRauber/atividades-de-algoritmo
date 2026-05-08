85040f5a86d188105b8fc30d4ef5b7a00287949d
Eduardo Rauber Silva <1658928@sga.pucminas.br>
2026-05-08 07:59:32 -0300

    Finalização do ex3 da AT20

--- estudos/AT20/ex2.py
+++ estudos/AT20/ex2.py
@@ -1,13 +1,18 @@
 from ex1 import pontuacao_jogadores as funcao
 
-jogador=[]
-jogador = funcao(jogador)
-media_partida = []
+def calcular_media(jogador, media_partida):
+    for i in range(4):
+        soma = 0
+        for j in range(3):
+            soma += jogador[j][i]
+        media = soma / 3
+        media_partida.append(media)
+        if __name__ == "__main__":
+            print(f"media da partida {i+1}: {media}")
+    return media_partida
 
-for i in range(4):
-    soma = 0
-    for j in range(3):
-        soma += jogador[j][i]
-    media = soma / 3
-    media_partida.append(media)
-    print(f"media da partida {i+1}: {media}")
+if __name__ == "__main__":
+    jogador=[]
+    jogador = funcao(jogador)
+    media_partida = []
+    calcular_media(jogador, media_partida)
--- estudos/AT20/ex3.py
+++ estudos/AT20/ex3.py
@@ -1,9 +1,15 @@
-import ex2 as funcoes
+from ex1 import pontuacao_jogadores as pontuacao
+from ex2 import calcular_media as media
+
+jogador = []
+jogador = pontuacao(jogador)
+media_partida = []
+media_partida = media(jogador, media_partida)
 
 for i in range(4):
     if i == 0 or maior < media_partida[i]:
         maior = media_partida[i]
         num_partida = i
 
-print(f"A partida {num_partida+1} teve melhor desempenho em média. /n Desempenho médio = {maior}")
+print(f"A partida {num_partida+1} teve melhor desempenho em média. \n Desempenho médio = {maior}")
     
